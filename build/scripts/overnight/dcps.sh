cf816cb6c57023443d03ac6c3c59f46171313ab3
Paul Gallagher <pg@prismtech.com>
2009-03-23 16:56:52 +0000

    scdds1295 added rbt tests and new log structure [pg]

--- build/scripts/overnight/dcps.sh
+++ build/scripts/overnight/dcps.sh
@@ -21,14 +21,15 @@ setupBuildInfo()
    echo "SETUP_TYPE=$SETUP_TYPE" >> $LOGDIR/BUILDINFO
    echo "BUILD_DIST=$BUILD_DIST" >> $LOGDIR/BUILDINFO
    echo "KEEP_DIST=$KEEP_DIST" >> $LOGDIR/BUILDINFO
-   echo "RUN_TESTS=$RUN_TESTS" >> $LOGDIR/BUILDINFO
+   echo "RUN_DBT=$RUN_DBT" >> $LOGDIR/BUILDINFO
+   echo "RUN_RBT=$RUN_RBT" >> $LOGDIR/BUILDINFO
    echo "RUN_EXAMPLES=$RUN_EXAMPLES" >> $LOGDIR/BUILDINFO
    echo "REV=$REV" >> $LOGDIR/BUILDINFO
    echo "VER=$VER" >> $LOGDIR/BUILDINFO
    echo "DEP=$DEP" >> $LOGDIR/BUILDINFO
 
    # List the fields we want the display page to show.
-   echo "BUILD_DISPLAY=/SETUP_TYPE,/KEEP_DIST,RUN/TESTS,RUN/EXAMPLES" >> $LOGDIR/BUILDINFO
+   echo "BUILD_DISPLAY=/SETUP_TYPE,/BUILD_DIST,/KEEP_DIST,BUILD/DBT,BUILD/RBT,RUN/DBT,RUN/RBT,RUN/EXAMPLES" >> $LOGDIR/BUILDINFO
 }
 
 ProcessArgs $*
@@ -44,7 +45,12 @@ WaitForParentBuild
 
 cat > $LOGDIR/LOGFILES <<EOF
 BUILD=build.txt
-TESTS=dbt.txt
+BUILD_DBT=build-dbt-tests.txt
+BUILD_RBT=build-rbt-tests.txt
+RUN_DBT=perform-dbt-tests.txt
+RUN_RBT=perform-rbt-tests.txt
+BUILD_DIST=build-dist.txt
+KEEP_DIST=archive-dist.txt
 EOF
 
 cat > $RESFILE <<EOF
@@ -63,36 +69,73 @@ then
 else
     echo "KEEP_DIST=SKIP" >> $RESFILE
 fi
-if [ $RUN_TESTS = "yes" ]
+if [ $RUN_DBT = "yes" ]
 then
-    echo "TESTS/DBT=TODO" >> $RESFILE
+    echo "BUILD/DBT=TODO" >> $RESFILE
+    echo "RUN/DBT=TODO" >> $RESFILE
 else
-    echo "TESTS/DBT=SKIP" >> $RESFILE
+    echo "BUILD/DBT=SKIP" >> $RESFILE
+    echo "RUN/DBT=SKIP" >> $RESFILE
+fi
+if [ $RUN_RBT = "yes" ]
+then
+    echo "BUILD/RBT=TODO" >> $RESFILE
+    echo "RUN/RBT=TODO" >> $RESFILE
+else
+    echo "BUILD/RBT=SKIP" >> $RESFILE
+    echo "RUN/RBT=SKIP" >> $RESFILE
 fi
 ArchiveLogs
 
-./dcps_build $ARGS
+./dcps_build $ARGS > $LOGDIR/build.txt 2>&1
 LAST_STAGE_WORKED=$?
 
 ArchiveLogs
 
 if [ $LAST_STAGE_WORKED = 0 ]
 then
-    ./dcps_build_dist $ARGS
+    ./dcps_build_dist $ARGS > $LOGDIR/build-dist.txt 2>&1
+    LAST_STAGE_WORKED=$?
+
+    if test_build_dist $LOGDIR/build-dist.txt $LAST_STAGE_WORKED
+    then
+        echo "BUILD_DIST=PASS" >> $RESFILE
+    else
+        echo "BUILD_DIST=FAIL" >> $RESFILE
+        LAST_STAGE_WORKED=1
+    fi
+    ArchiveLogs
+fi
+
+if [ $LAST_STAGE_WORKED = 0 ]
+then
+    ./dcps_archive_dist $ARGS > $LOGDIR/archive-dist.txt 2>&1
     LAST_STAGE_WORKED=$?
     ArchiveLogs
 fi
 
 if [ $LAST_STAGE_WORKED = 0 ]
 then
-    ./dcps_archive_dist $ARGS
+    ./dcps_build_dbt_tests $ARGS > $LOGDIR/build-dbt-tests.txt 2>&1
+    LAST_STAGE_WORKED=$?
+    ArchiveLogs
+fi
+if [ $LAST_STAGE_WORKED = 0 ]
+then
+    ./dcps_perform_dbt_tests $ARGS > $LOGDIR/perform-dbt-tests.txt 2>&1
     LAST_STAGE_WORKED=$?
     ArchiveLogs
 fi
 
 if [ $LAST_STAGE_WORKED = 0 ]
 then
-    ./dcps_perform_tests $ARGS
+    ./dcps_build_rbt_tests $ARGS > $LOGDIR/build-rbt-tests.txt 2>&1
+    LAST_STAGE_WORKED=$?
+    ArchiveLogs
+fi
+if [ $LAST_STAGE_WORKED = 0 ]
+then
+    ./dcps_perform_rbt_tests $ARGS > $LOGDIR/perform-rbt-tests.txt 2>&1
     LAST_STAGE_WORKED=$?
     ArchiveLogs
 fi
