bbc5c90c2920294ea5e15e1bd794f53d31ea90f1
Paul Gallagher <pg@prismtech.com>
2009-03-27 18:13:52 +0000

    scdds1295 added more checking to overnight runs [pg]

--- build/scripts/overnight/dcps.sh
+++ build/scripts/overnight/dcps.sh
@@ -44,6 +44,7 @@ ArchiveLogs
 
 WaitForParentBuild
 
+#Logs to use when there is a fail
 cat > $LOGDIR/LOGFILES <<EOF
 BUILD=build.txt
 BUILD_DBT=build-dbt-tests.txt
@@ -52,6 +53,17 @@ RUN_DBT=perform-dbt-tests.txt
 RUN_RBT=perform-rbt-tests.txt
 BUILD_DIST=build-dist.txt
 KEEP_DIST=archive-dist.txt
+EOF
+
+#Logs to use when pass
+cat > $LOGDIR/LOGFILES_PASSED <<EOF
+BUILD=build.txt
+BUILD_DBT=build-dbt-tests.txt
+BUILD_RBT=build-rbt-tests.txt
+RUN_DBT=perform-dbt-tests.txt
+RUN_RBT=perform-rbt-tests.txt
+BUILD_DIST=build-dist.txt
+KEEP_DIST=../distro
 EOF
 
 cat > $RESFILE <<EOF
@@ -90,6 +102,13 @@ ArchiveLogs
 
 ./dcps_build $ARGS > $LOGDIR/build.txt 2>&1
 BUILD_STAGE_WORKED=$?
+if test_build $LOGDIR/build.txt $BUILD_STAGE_WORKED
+then
+    echo "BUILD=PASS" >> $RESFILE
+else
+    echo "BUILD=FAIL" >> $RESFILE
+    BUILD_DIST_STAGE_WORKED=1
+fi
 ArchiveLogs
 
 if [ $BUILD_STAGE_WORKED = 0 ]
@@ -98,50 +117,82 @@ then
     BUILD_DIST_STAGE_WORKED=$?
     if test_build_dist $LOGDIR/build-dist.txt $BUILD_DIST_STAGE_WORKED
     then
-        echo "BUILD_DIST=PASS" >> $RESFILE
+        echo "BUILD_DIST=PASS" >> $RESFILE        
         ./dcps_archive_dist $ARGS > $LOGDIR/archive-dist.txt 2>&1
         ARCHIVE_STAGE_WORKED=$?
+        if test_archive_dist $LOGDIR/archive-dist.txt $ARCHIVE_STAGE_WORKED
+        then
+            echo "KEEP_DIST=PASS" >> $RESFILE
+        else
+            echo "KEEP_DIST=FAIL" >> $RESFILE
+            ARCHIVE_STAGE_WORKED=1
+        fi
     else
+        echo "KEEP_DIST=ABORTED" >> $RESFILE
+        ARCHIVE_STAGE_WORKED=1
         echo "BUILD_DIST=FAIL" >> $RESFILE
         BUILD_DIST_STAGE_WORKED=1
     fi
     ArchiveLogs
 
-    ./dcps_build_dbt_tests $ARGS > $LOGDIR/build-dbt-tests.txt 2>&1
-    BUILD_DBT_STAGE_WORKED=$?
-    if test_build_dbt_tests $LOGDIR/build-dbt-tests.txt $BUILD_DBT_STAGE_WORKED
+    if [ "$RUN_DBT" != "yes" ]
     then
-        echo "BUILD/DBT=PASS" >> $RESFILE
+        echo "BUILD/DBT=SKIPPED" >> $RESFILE
+        echo "RUN/DBT=SKIPPED" >> $RESFILE
+        BUILD_DBT_STAGE_WORKED=0
+        PERFORM_DBT_STAGE_WORKED=0
     else
-        echo "BUILD/DBT=FAIL" >> $RESFILE
-        BUILD_DBT_STAGE_WORKED=1
+        ./dcps_build_dbt_tests $ARGS > $LOGDIR/build-dbt-tests.txt 2>&1
+        BUILD_DBT_STAGE_WORKED=$?
+        if test_build_dbt_tests $LOGDIR/build-dbt-tests.txt $BUILD_DBT_STAGE_WORKED
+        then
+            echo "BUILD/DBT=PASS" >> $RESFILE
+        else
+            echo "BUILD/DBT=FAIL" >> $RESFILE
+            BUILD_DBT_STAGE_WORKED=1
+        fi
+        ArchiveLogs
+        
+        ./dcps_perform_dbt_tests $ARGS > $LOGDIR/perform-dbt-tests.txt 2>&1
+        PERFORM_DBT_STAGE_WORKED=$?
+        if test_perform_dbt_tests $LOGDIR/DBT-Results $PERFORM_DBT_STAGE_WORKED
+        then
+            echo "RUN/DBT=PASS" >> $RESFILE
+        else
+            echo "RUN/DBT=$DBT_INFO" >> $RESFILE
+            PERFORM_DBT_STAGE_WORKED=1
+        fi
+        ArchiveLogs
     fi
-    ArchiveLogs
 
-    ./dcps_perform_dbt_tests $ARGS > $LOGDIR/perform-dbt-tests.txt 2>&1
-    PERFORM_DBT_STAGE_WORKED=$?
-    if test_perform_dbt_tests $LOGDIR/DBT-Results $PERFORM_DBT_STAGE_WORKED
+    if [ "$RUN_RBT" != "yes" ]
     then
-        echo "RUN/DBT=PASS" >> $RESFILE
+        echo "BUILD/RBT=SKIPPED" >> $RESFILE
+        echo "RUN/RBT=SKIPPED" >> $RESFILE
+        BUILD_RBT_STAGE_WORKED=0
+        PERFORM_RBT_STAGE_WORKED=0
     else
-        echo "RUN/DBT=$DBT_INFO" >> $RESFILE
-        PERFORM_DBT_STAGE_WORKED=1
+        ./dcps_build_rbt_tests $ARGS > $LOGDIR/build-rbt-tests.txt 2>&1
+        BUILD_RBT_STAGE_WORKED=$?
+        if test_build_rbt_tests $LOGDIR/RBT-Results $BUILD_RBT_STAGE_WORKED
+        then
+            echo "BUILD/RBT=PASS" >> $RESFILE
+        else
+            echo "BUILD/RBT=$RBT_INFO" >> $RESFILE
+            BUILD_RBT_STAGE_WORKED=1
+        fi
+        ArchiveLogs
+        ./dcps_perform_rbt_tests $ARGS > $LOGDIR/perform-rbt-tests.txt 2>&1
+        PERFORM_RBT_STAGE_WORKED=$?
+        if test_perform_rbt_tests $LOGDIR/RBT-Results $PERFORM_RBT_STAGE_WORKED
+        then
+            echo "RUN/RBT=PASS" >> $RESFILE
+        else
+            echo "RUN/RBT=$RBT_INFO" >> $RESFILE
+            PERFORM_RBT_STAGE_WORKED=1
+        fi
+        ArchiveLogs
     fi
-    ArchiveLogs
-    
-    ./dcps_build_rbt_tests $ARGS > $LOGDIR/build-rbt-tests.txt 2>&1
-    BUILD_RBT_STAGE_WORKED=$?
-    ArchiveLogs
-    ./dcps_perform_rbt_tests $ARGS > $LOGDIR/perform-rbt-tests.txt 2>&1
-    PERFORM_RBT_STAGE_WORKED=$?
-    if test_perform_rbt_tests $LOGDIR/RBT-Results $PERFORM_RBT_STAGE_WORKED
-    then
-        echo "RUN/RBT=PASS" >> $RESFILE
-    else
-        echo "RUN/RBT=$RBT_INFO" >> $RESFILE
-        PERFORM_RBT_STAGE_WORKED=1
-    fi
-    ArchiveLogs
 fi
 
 if [ "$BUILD_STAGE_WORKED" = 0 ]
