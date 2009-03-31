ba623c9167bb8ee57d103a316126cd8e1029bdf1
Paul Gallagher <pg@prismtech.com>
2009-03-31 16:58:35 +0100

    scdds#1295 added rhel5 machine and moved some java to the outer ring [pg]

--- src/api/dcps/java/code/org/opensplice/dds/dcps/DataReaderListener.java
+++
@@ -1,7 +0,0 @@
-package org.opensplice.dds.dcps;
-
-/**@interface DataReaderListener
- * @brief A Listener that can be attached to a DataReader and can be used
- * to notify status changes of a DataReader.
- */
-public interface DataReaderListener extends DCPSListener{}
