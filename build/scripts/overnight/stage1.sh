a75d71f62e3831fafa3b2c810772f7c04261ede1
James Butcher <jb@prismtech.com>
2009-04-14 16:02:57 +0100

    scdds#1387 rework of config files to allow individual override of env variables per machine basis

--- build/scripts/overnight/stage1.sh
+++ build/scripts/overnight/stage1.sh
@@ -4,17 +4,6 @@ cd `dirname $0` || exit 1
 
 IBSDIR=`pwd`
 
-######## MIN NEEDED FOR OSPLI HACK HACK REMOVE
-T_UNAME=`uname`
-if [ "$T_UNAME" = "Linux" ]
-then
-    . /opt/ospldev/gcc/v3.2.3/release.com
-    export JAVA_HOME=/home/dds/INSTALLED_FOR_DDS/JAVA
-    export TAO_ROOT=/home/dds/INSTALLED_FOR_DDS/TAO
-    export EORBHOME=/home/dds/INSTALLED_FOR_DDS/eorb
-fi
-#############################
-
 . buildsys/functions
 . ./dcps_functions
 
