27ca5bdf41c6c681c8d5d26136c2e1dd3b43232c
Philip Conroy <pac_at_prismtech.com>
2009-04-27 16:53:35 +0100

    scdds#1408 Moved mmstat from outer ring.

---
+++ src/tools/mmstat/vxworks/code/mm_trc.h
@@ -0,0 +1,39 @@
+/*
+ *                         OpenSplice DDS
+ *
+ *   This software and documentation are Copyright 2006 to 2009 PrismTech 
+ *   Limited and its licensees. All rights reserved. See file:
+ *
+ *                     $OSPL_HOME/LICENSE 
+ *
+ *   for full copyright notice and license terms. 
+ *
+ */
+#ifndef MM_TRC_H
+#define MM_TRC_H
+
+#include <c_typebase.h>
+#include <v_entity.h>
+
+C_CLASS(monitor_trc);
+#define monitor_trc(o)     ((monitor_trc)(o))
+
+monitor_trc
+monitor_trcNew (
+    c_long objectCountLimit,
+    const char *filterExpression,
+    c_bool delta
+    );
+
+void
+monitor_trcFree (
+    monitor_trc o
+    );
+
+void
+monitor_trcAction (
+    v_entity entity,
+    c_voidp args
+    );
+
+#endif /* MM_TRC_H */
