b72674168bc390b149a3b9970fcab12843a4ad92
Reinier Torenbeek <reinier.torenbeek@prismtech.com>
2009-05-14 16:10:48 -0400

    scdds#1380: Fixed bug on multiple delete_xxx calls, leading to an UNHANDLED EXCEPTION message

--- src/api/dcps/ccpp/code/ccpp_Subscriber_impl.cpp
+++ src/api/dcps/ccpp/code/ccpp_Subscriber_impl.cpp
@@ -1,12 +1,12 @@
 /*
  *                         OpenSplice DDS
  *
- *   This software and documentation are Copyright 2006 to 2009 PrismTech 
+ *   This software and documentation are Copyright 2006 to 2009 PrismTech
  *   Limited and its licensees. All rights reserved. See file:
  *
- *                     $OSPL_HOME/LICENSE 
+ *                     $OSPL_HOME/LICENSE
  *
- *   for full copyright notice and license terms. 
+ *   for full copyright notice and license terms.
  *
  */
 #include <gapi.h>
@@ -184,6 +184,7 @@ DDS::ReturnCode_t DDS::Subscriber_impl::delete_datareader (
       result = gapi_subscriber_delete_datareader(_gapi_self, dataReader->_gapi_self);
       if (result == DDS::RETCODE_OK)
       {
+        dataReader->_gapi_self = NULL;
         if (myUD)
         {
           delete myUD;
