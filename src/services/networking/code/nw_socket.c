b4fa25bccc0c251bda37e2aa8f2c5d58049d87bf
Reinier Torenbeek <reinier.torenbeek@prismtech.com>
2009-06-03 11:41:41 -0400

    scdds#1380: fixed setting of SO_REUSEADDR which was set to 0 in stead of SK_TRUE.

--- src/services/networking/code/nw_socket.c
+++ src/services/networking/code/nw_socket.c
@@ -370,7 +370,7 @@ nw_socketBind(
     if (sock != NULL) {
         /* Avoid already in use error messages */
         optLen = (os_uint32)sizeof(optVal);
-        optVal = 0;
+        optVal = SK_TRUE;
         retVal = os_sockSetsockopt(sock->socketData,
                             SOL_SOCKET, SO_REUSEADDR,
                             (void *)&optVal, optLen);
