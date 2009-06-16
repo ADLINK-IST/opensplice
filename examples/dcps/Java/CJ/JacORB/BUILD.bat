faec6e73255bb05fccd694fbb89d88d6e1fdc57a
Emiel Schuurman <emiel.schuurman@prismtech.com>
2009-06-16 14:10:36 +0200

    scdds1440: Added pingpong example for the CJ api and fixed the way the dcpscj.jar is built

---
+++ examples/dcps/Java/CJ/JacORB/BUILD.bat
@@ -0,0 +1,39 @@
+@ECHO OFF
+
+MD bld
+CD bld
+MD idlpp
+CD idlpp
+
+ECHO "Compiling IDL with SPLICE IDL compiler"
+idlpp -l java -C ../../pingpong.idl
+
+ECHO "Compiling IDL with JacORB IDL compiler"
+java -classpath %JACORB_HOME%/lib/idl.jar:%JACORB_HOME%/lib/logkit-1.2.jar:%CLASSPATH% org.jacorb.idl.parser ../../pingpong.idl
+
+ECHO "Compiling generated java code"
+javac -classpath ".;%OSPL_HOME%/jar/dcpssaj.jar" -d .. pingpong\*.java
+IF NOT ERRORLEVEL==0 (
+  ECHO:
+  ECHO Compilation failed
+  ECHO:
+  GOTO error
+)
+
+CD ..
+
+ECHO "Compiling application java code"
+javac -classpath ".;%OSPL_HOME%/jar/dcpssaj.jar" -d . ..\*.java
+IF NOT ERRORLEVEL==0 (
+  ECHO:
+  ECHO Compilation failed
+  ECHO:
+  GOTO error
+)
+
+CD ..
+GOTO end
+
+:error
+ECHO An error occurred, exiting now
+:end
