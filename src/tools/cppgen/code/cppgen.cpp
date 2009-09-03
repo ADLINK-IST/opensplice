b15e0ef2fa93edb1430334a26335e6168b7685ef
George Varsamis <gv@prismtech.com>
2009-09-03 18:02:12 +0100

    scdds#1570 added Copyright notices

--- src/tools/cppgen/code/cppgen.cpp
+++ src/tools/cppgen/code/cppgen.cpp
@@ -1,3 +1,15 @@
+/*
+ *                         OpenSplice DDS
+ *
+ *   This software and documentation are Copyright 2006 to 2009 PrismTech 
+ *   Limited and its licensees. All rights reserved. See file:
+ *
+ *                     $OSPL_HOME/LICENSE 
+ *
+ *   for full copyright notice and license terms. 
+ *
+ */
+
 #include <drv_extern.h>
 
 int main(int argc, char **argv)
--- src/tools/cppgen/code/xbe_opnameset.cpp
+++ src/tools/cppgen/code/xbe_opnameset.cpp
@@ -1,3 +1,14 @@
+/*
+ *                         OpenSplice DDS
+ *
+ *   This software and documentation are Copyright 2006 to 2009 PrismTech 
+ *   Limited and its licensees. All rights reserved. See file:
+ *
+ *                     $OSPL_HOME/LICENSE 
+ *
+ *   for full copyright notice and license terms. 
+ *
+ */
 #include "xbe_opnameset.h"
 
 DDSHashValue std_strhash(const DDS_StdString& str)
