60d594e20cc2081d91d3286b53f5d29f4a228ea1
George Varsamis <gv@prismtech.com>
2009-09-04 10:42:51 +0100

    scdds#1570 Added Copyright notice to files

--- src/tools/cppgen/include/ast_hdrs.h
+++ src/tools/cppgen/include/ast_hdrs.h
@@ -1,3 +1,14 @@
+/*
+ *                         OpenSplice DDS
+ *
+ *   This software and documentation are Copyright 2006 to 2009 PrismTech 
+ *   Limited and its licensees. All rights reserved. See file:
+ *
+ *                     $OSPL_HOME/LICENSE 
+ *
+ *   for full copyright notice and license terms. 
+ *
+ */
 #ifndef _SUN_HDRS_HH_
 #define _SUN_HDRS_HH_
 
--- src/tools/cppgen/include/fe_value_inheritance_spec.h
+++ src/tools/cppgen/include/fe_value_inheritance_spec.h
@@ -1,3 +1,14 @@
+/*
+ *                         OpenSplice DDS
+ *
+ *   This software and documentation are Copyright 2006 to 2009 PrismTech 
+ *   Limited and its licensees. All rights reserved. See file:
+ *
+ *                     $OSPL_HOME/LICENSE 
+ *
+ *   for full copyright notice and license terms. 
+ *
+ */
 #ifndef _FE_VALUE_INHERITANCE_SPEC_HH
 #define _FE_VALUE_INHERITANCE_SPEC_HH
 
